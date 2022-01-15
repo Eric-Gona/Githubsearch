8de64a55e14fea069b6ee8b37f0efafd3fa7d270
Eric-Gona <eric.gona254@gmail.com>
2022-01-15 14:17:43 +0300

    adds app,module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
-import { SearchComponent } from './search/search.component';
+import { GitsearchComponent, SearchComponent } from './search/search.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UserSService } from './user-s.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    SearchComponent
+    SearchComponent,
+    GitsearchComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [UserSService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
